b8bee4934581d01e96074ff33cc43fd5d47885f7
Miquel Llufriu <miguelllufriu@gmail.com>
2019-03-19 21:26:29 +0100

    Hide API key for security reasons

--- src/components/MapWrapper.jsx
+++ src/components/MapWrapper.jsx
@@ -1,21 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 
 export function MapWrapper() {
     
-    const TOKEN = 'pk.eyJ1IjoibWlybWlsaW4iLCJhIjoiY2p0YmdwZTQxMGtkYjN5bzNneHVnaHF2NyJ9.vYHU4-q6NWicDd0H6JSElg';
+    const TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
     
     const [viewport, useViewport] = useState({
         width: 800,
         height: 800,
-        latitude: 40.7577,
-        longitude: 3.4376,
+        latitude: 40.0077,
+        longitude: 3.8376,
         zoom: 8
       });
-    useEffect(() => {
-        console.log(viewport)
-    });
+
     return (
         <ReactMapGL
         {...viewport}
